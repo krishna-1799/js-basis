b38963a6f09c968ddb2f7009c5bee072b43519fb
krishna <saikrishnakambampati17@gmail.com>
2022-11-15 17:34:18 +0000

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,44 @@
+// console .log('hello');
+// n=10;
+
+// //const a=10;
+// //const b=20;
+// //console.log('a is ' + a) ;
+// //if (a>b)//
+// {
+//     //console.log('a is greater')
+// }
+// //else
+// {
+//     //console.log('b is greater')
+// }
+// //for (let i = 0; i< n; i++) 
+// {
+//    // console.log(i)
+// }
+// function print('x');
+
+// {
+//     console .log('x')
+// }
+// print ('x');
+
+
+const cowsay = require("cowsay");
+var oneLinerJoke = require('one-liner-joke');
+var getRandomJoke = oneLinerJoke.getRandomJoke();
+
+console.log(cowsay.say({
+    text :getRandomJoke.body,
+    e : "oO",
+    T : "U "
+}));
+
+// or cowsay.think()
+// var oneLinerJoke = require('one-liner-joke');
+
+
+// var getRandomJoke = oneLinerJoke.getRandomJoke();
+// console.log(getRandomJoke)
+
+
